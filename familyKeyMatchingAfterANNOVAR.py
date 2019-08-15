690497591541cbec91cd6a320a68f3e69c2e2263
speleonut <37854406+speleonut@users.noreply.github.com>
2019-08-15 18:43:20 +0930

    Add new python family filtering script

---
+++ familyKeyMatchingAfterANNOVAR.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+
+# Script to filter affected family members for matched genotypes in a multisample ANNOVAR file
+import pandas as pd
+import sys, getopt, csv
+
+def usage():
+    print(
+'''
+# familyKeyMatchingAfterANNOVAR.py a script to filter affected family members for matched genotypes in a multisample ANNOVAR file
+# also outputting a BestGeneCandidates file.  
+#
+# Usage familyKeyMatchingAfterANNOVAR.py -i ANNOVAR.table.txt -s sampleList.txt [-k] | [ -h | --help ]
+#
+# Options:
+# -i           /path/to/inputFile    REQUIRED: A multisample ANNOVAR table in tab delimited format
+# -s           /path/to/sampleFile   OPTIONAL: A list of specific samples to extract. By default all samples are assumed affected and this might not be what you want
+# -h | --help  Displays help                 OPTIONAL: Displays usage information.
+#
+# Script created by Mark Corbett on 15/08/2019
+# Contact: mark.corbett at adelaide.edu dot au
+# Edit History (Name; Date; Description)
+#
+'''
+         )
+
+# Set initial values
+inputFile = ''
+sampleFile = ''
+notGeneTerms = ['downstream', 'intergenic', 'intronic', 'ncRNA_exonic', 'ncRNA_intronic', 'ncRNA_splicing', 'ncRNA_UTR3', 'ncRNA_UTR5', 'upstream', 'UTR3', 'UTR5']
+
+# Read command line arguments
+try:
+    opts, args = getopt.getopt(sys.argv[1:],'hi:s:',['help'])
+except getopt.GetoptError:
+    usage
+    sys.exit(2)
+for opt, arg in opts:
+    if opt in ("-h", "--help"):
+        usage()
+        sys.exit()
+    elif opt in ("-i"):
+        inputFile = arg
+    elif opt in ("-s"):
+        sampleFile = arg
+
+# Make sure you have what you need
+if inputFile == '':
+    usage()
+    print('Hey, you forgot to tell me which ANNOVAR file to filter\n')
+    sys.exit(2)    
+
+# Count the number of columns in the ANNOVAR table
+with open(inputFile) as f:
+    reader = csv.reader(f, delimiter='\t', skipinitialspace=True)
+    first_row = next(reader)
+    num_cols = len(first_row)-1 # use -1 to set the numbering to 0 based
+
+# Open ANNOVAR table with pandas setting the chr-start-ref-obs column as the index
+ANNOVARtable=pd.read_csv(inputFile, sep='\t', index_col = num_cols) 
+coreTable=ANNOVARtable.loc[:,:'FORMAT']
+
+if sampleFile =='':
+    print('INFO: Using all samples available in the file\n')
+    samples = ANNOVARtable.columns[ANNOVARtable.columns.get_loc("FORMAT")+1:]
+elif sampleFile !='':
+    samples = [line.rstrip() for line in open(sampleFile)]
+
+dfCore=coreTable
+for s in samples:
+    currentSampleList=ANNOVARtable[[s]]
+    homList=currentSampleList[currentSampleList[s].str.match('1/1')]
+    dfCore = pd.concat([dfCore,homList], axis=1, join='inner') # Add , sort='False' once Ubuntu is upgraded
+
+dfCore.to_csv("ibdAndXl."+inputFile, sep='\t')
+bgc=dfCore[~dfCore.Func.gene.isin(notGeneTerms)]
+bgc.to_csv("ibdAndXl.BestGeneCandidates."+inputFile, sep='\t')
+
+dfCore=coreTable
+for s in samples:
+    currentSampleList=ANNOVARtable[[s]]
+    homList=currentSampleList[currentSampleList[s].str.match('0/1')]
+    dfCore = pd.concat([dfCore,homList], axis=1, join='inner') # Add , sort='False' once Ubuntu is upgraded
+
+dfCore.to_csv("het."+inputFile, sep='\t')
+bgc=dfCore[~dfCore.Func.gene.isin(notGeneTerms)]
+bgc.to_csv("het.BestGeneCandidates."+inputFile, sep='\t')
